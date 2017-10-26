b828d1043426033fb79f8c3aec97a9c20275f734
thery <sylvain.thery@icube.unistra.fr>
2017-10-27 01:34:57 +0200

    travis +mac

---
+++ .travis_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+ sudo apt-get update
+ sudo apt-get install "g++-5"
+ sudo apt-get install cmake3
+ sudo apt-get install zlib1g-dev
+ sudo apt-get install libpng12-dev
+ sudo apt-get install libopenexr-dev 
+ pushd /tmp/
+ wget http://igg.unistra.fr/people/thery/itk.tgz
+ cd /usr/
+ sudo tar xf /tmp/itk.tgz
+ popd
+fi
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+ brew install insighttoolkit
+ brew install openexr
+ brew install libpng
+fi
+ 
