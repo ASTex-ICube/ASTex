020d51ca2e7ba9d118b66ad70973db046704f780
sylvainthery <sylvain.thery@icube.unistra.fr>
2017-10-27 12:51:04 +0200

    travis ....

--- .travis_install.sh
+++ .travis_install.sh
@@ -8,7 +8,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
  sudo apt-get install libpng12-dev
  sudo apt-get install libopenexr-dev 
  pushd /tmp/
- wget http://igg.unistra.fr/people/thery/itk.tgz
+ wget https://mycore.core-cloud.net/index.php/s/BfSTlzfQIDqvI8l/download -O itk.tgz
  cd /usr/
  sudo tar xf /tmp/itk.tgz
  popd
@@ -16,7 +16,7 @@ fi
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
  pushd /tmp/
- wget https://mycore.core-cloud.net/index.php/s/jCZ3rakI3t22JSB/download -O itk_osx.tgz
+ wget https://mycore.core-cloud.net/index.php/s/CSKCKkV1uPAWoJT/download -O itk_osx.tgz
  cd /usr/local
  sudo tar xf /tmp/itk_osx.tgz
  popd
