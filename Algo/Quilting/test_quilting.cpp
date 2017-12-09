c2c8f3a19a1fd5b3860b080ed881b3c38f681b1d
thery <sylvain.thery@icube.unistra.fr>
2017-12-09 23:02:00 +0100

    minor

--- Algo/Quilting/test_quilting.cpp
+++ Algo/Quilting/test_quilting.cpp
@@ -35,16 +35,8 @@ int main(int argc, char** argv)
 	else
 		q.loadInput(TEMPO_PATH+"quilting_input.png");
 
-//	std::cout << "Compute random"<< std::endl;
-//	q.computeRandom(1024,1024,64);
-//	q.saveOutput(TEMPO_PATH+"quilting_random_output.png");
-
-//	std::cout << "Compute fitting"<< std::endl;
-//	q.computeFittingTiles(1024,1024,64,8,3);
-//	q.saveOutput(TEMPO_PATH+"quilting_fitting_output.png");
-
 	std::cout << "Compute fitting + cutting"<< std::endl;
-	q.computeFittingTilesPathCut(1024,1024,64,16,5,4096);
+	q.computeFittingTilesPathCut(2048,2048,64,16,5,4096);
 	q.saveOutput(TEMPO_PATH+"quilting_path_output.png");
 
 
