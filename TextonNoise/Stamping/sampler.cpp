61cdcf23bfb2572c16a777cbf286758116104e12
Nicolas Lutz <nicolas.lutz@icube.unistra.fr>
2018-03-16 11:19:20 +0100

    Moved PoissonSampler generate() function into sampler.cpp

--- TextonNoise/Stamping/sampler.cpp
+++ TextonNoise/Stamping/sampler.cpp
@@ -36,6 +36,67 @@ std::vector<Eigen::Vector2f> UniformSampler::generate()
     return SamplePoints;
 }
 
+std::vector<Eigen::Vector2f> PoissonSampler::generate()
+{
+    if ( m_minDistance < 0.0f )
+    {
+        m_minDistance = sqrt( float(m_nbPoints) ) / float(m_nbPoints);
+    }
+
+    std::vector<Eigen::Vector2f> SamplePoints;
+    std::vector<Eigen::Vector2f> ProcessList;
+
+    // create the grid
+    float CellSize = m_minDistance / sqrt( 2.0f );
+
+    int GridW = ( int )ceil( 1.0f / CellSize );
+    int GridH = ( int )ceil( 1.0f / CellSize );
+
+    sGrid Grid( GridW, GridH, CellSize );
+
+    Eigen::Vector2f FirstPoint;
+    do {
+        FirstPoint = Eigen::Vector2f( m_generator.RandomFloat(), m_generator.RandomFloat() );
+    } while (!(m_generateInCircle ? IsInCircle(FirstPoint) : IsInRectangle(FirstPoint)));
+
+    // update containers
+    ProcessList.push_back( FirstPoint );
+    SamplePoints.push_back( FirstPoint );
+    Grid.Insert( FirstPoint );
+
+    // generate new points for each point in the queue
+    while ( !ProcessList.empty() && SamplePoints.size() < m_nbPoints )
+    {
+#if POISSON_PROGRESS_INDICATOR
+        // a progress indicator, kind of
+        if ( SamplePoints.size() % 100 == 0 ) std::cout << ".";
+#endif // POISSON_PROGRESS_INDICATOR
+
+        Eigen::Vector2f Point = PopRandom( ProcessList, m_generator );
+
+        for ( int i = 0; i < m_newPointsCount; i++ )
+        {
+            Eigen::Vector2f NewPoint = GenerateRandomPointAround( Point, m_minDistance, m_generator );
+
+            bool Fits = m_generateInCircle ? IsInCircle(NewPoint) : IsInRectangle(NewPoint);
+
+            if ( Fits && !Grid.IsInNeighbourhood( NewPoint, m_minDistance, CellSize ) )
+            {
+                ProcessList.push_back( NewPoint );
+                SamplePoints.push_back( NewPoint );
+                Grid.Insert( NewPoint );
+                continue;
+            }
+        }
+    }
+
+#if POISSON_PROGRESS_INDICATOR
+    std::cout << std::endl << std::endl;
+#endif // POISSON_PROGRESS_INDICATOR
+
+    return SamplePoints;
+}
+
 } //namsepace Stamping
 
 } //namespace ASTex
