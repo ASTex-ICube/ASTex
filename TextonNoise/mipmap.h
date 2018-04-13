8eb28d23d28758ee71dae20f15f435bb7ae6eb03
Nicolas Lutz <nicolas.lutz@icube.unistra.fr>
2018-04-13 11:29:07 +0200

    Added several new ways to mipmap a texture.
    MipmapBooleanImage can be used to filter a boolean image,
    with the merging rule for A and B being (A || B).
    
    MipmapBitmask can be used to filter a bitmask image of any type,
    with the merging rule for A and B being (A | B).
    
    MipmapCEContent can be used to construct mipmaps out of
    a Mipmap class of the content (whithin a fully sized image)
    and a Mipmap class of the alpha of the patch (fully sized image).
    The result is a Mipmap class of the content, taking the space
    of the patch (it has the same content, but whithin a small space).
    All of these additions were made to filter the content exchange algorithm.
    
    Additionnaly, the function mipmap() now takes two unsigned ints.

--- TextonNoise/mipmap.h
+++ TextonNoise/mipmap.h
@@ -23,9 +23,19 @@ public:
 
     typedef enum {MIPMAP_ISO=0, MIPMAP_ANISO=1} mipmap_mode_t;
 
-    Mipmap(const I& texture, mipmap_mode_t mode=MIPMAP_ISO, int maxReductionLevel=0);
+    Mipmap();
+    Mipmap(const I& texture, mipmap_mode_t mode=MIPMAP_ISO, unsigned maxReductionLevel=0);
 
-    const I& mipmap(int xPowReduction, int yPowReduction);
+    //get
+
+    mipmap_mode_t mode() const {return m_mode;}
+    size_t numberMipmapsWidth() const;
+    size_t numberMipmapsHeight() const;
+    const I& mipmap(unsigned xPowReduction, unsigned yPowReduction) const;
+
+    //misc
+
+    virtual void generate(const I& texture, mipmap_mode_t mode=MIPMAP_ISO, unsigned maxPowReductionLevel=0);
 
     void fullMipmap(I& fullMipmap);
 
@@ -35,23 +45,37 @@ protected:
     virtual void filterDivide2Height(const I& texture, I& result);
     virtual void filterDivide2Full(const I& texture, I& result);
 
-private:
-
     std::vector<I> m_isoMipmaps; //< only isotropic mipmaps
     std::vector<std::vector<I>> m_anisoMipmapsWidth; //only anisotropic mipmaps which have reduced width compared to their isotropic counterparts
     std::vector<std::vector<I>> m_anisoMipmapsHeight; //only anisotropic mipmaps which have reduced height compared to their isotropic counterparts
 
     mipmap_mode_t m_mode;
+    bool m_generated;
 };
 
 template <class I>
-Mipmap<I>::Mipmap(const I& texture, mipmap_mode_t mode, int maxPowReductionLevel) :
-    m_mode(mode)
+Mipmap<I>::Mipmap() :
+    m_mode(MIPMAP_ISO),
+    m_generated(false)
+{}
+
+template <class I>
+Mipmap<I>::Mipmap(const I& texture, mipmap_mode_t mode, unsigned maxPowReductionLevel) :
+    m_mode(mode),
+    m_generated(true)
 {
+    generate(texture, mode, maxPowReductionLevel);
+}
+
+template <class I>
+void Mipmap<I>::generate(const I& texture, mipmap_mode_t mode, unsigned maxPowReductionLevel)
+{
+    m_generated = true;
+    m_mode=mode;
     //Resizing is done by computing the expected number of mipmaps and comparing it to the max number of mipmaps allowed.
 
-    m_isoMipmaps.resize(   maxPowReductionLevel<=0 ? std::floor( std::log2(std::min(texture.width(), texture.height()))+1 )
-                                                   : std::min( maxPowReductionLevel+1, int(std::floor(std::log2(std::min(texture.width(), texture.height()))))+1 )  );
+    m_isoMipmaps.resize(   maxPowReductionLevel==0 ? std::floor( std::log2(std::min(texture.width(), texture.height()))+1 )
+                                                   : std::min( maxPowReductionLevel+1, unsigned(std::floor(std::log2(std::min(texture.width(), texture.height()))))+1 )  );
 
     //Set the first mipmap to the given image
     m_isoMipmaps[0].initItk(texture.width(), texture.height());
@@ -67,7 +91,7 @@ Mipmap<I>::Mipmap(const I& texture, mipmap_mode_t mode, int maxPowReductionLevel
     {
         int powReductionLevel; //< sliding max number of reductions by 2, used to cap the reductions
         int w, h, indexIso; //< sliding width, sliding height, index in the isotropic mipmaps vector
-        bool cappedNbOfReductions=maxPowReductionLevel<=0; //< boolean determining if the number of reductions are capped, for readability
+        bool cappedNbOfReductions=maxPowReductionLevel==0; //< boolean determining if the number of reductions are capped, for readability
 
         //First triangular matrix is filled only with mipmaps whose content are mipmaps with successively divided widths compared to isotropic counterparts.
         //For instance, say I have a 256x256 texture (row number 0) with a 64x64 mipmap (row number 2), the columns would be 32x64 (column 0), 16x64 (column 1)...
@@ -75,7 +99,7 @@ Mipmap<I>::Mipmap(const I& texture, mipmap_mode_t mode, int maxPowReductionLevel
 
         powReductionLevel=maxPowReductionLevel;
         m_anisoMipmapsWidth.resize(cappedNbOfReductions     ? std::floor( std::log2(texture.height()) )
-                                                            : std::min ( maxPowReductionLevel, int(std::floor(std::log2(texture.height()))) ) );
+                                                            : std::min ( maxPowReductionLevel, unsigned(std::floor(std::log2(texture.height()))) ) );
         indexIso=0; //< index used to track the row of the matrix, so we know which isotropic mipmap should be used for the reduction.
         w=texture.width(), h=texture.height();
         //for each row of the matrix, do
@@ -120,16 +144,32 @@ Mipmap<I>::Mipmap(const I& texture, mipmap_mode_t mode, int maxPowReductionLevel
         }
 
     }
-
 }
 
 template <class I>
-const I& Mipmap<I>::mipmap(int xPowReduction, int yPowReduction)
+size_t Mipmap<I>::numberMipmapsWidth() const
 {
-    assert(xPowReduction>=0 && yPowReduction>=0 && "Mipmap::mipmap: xReduction and yReduction must be >= 0");
+    if(!m_generated)
+        return 0;
+    return m_mode == MIPMAP_ANISO ? m_anisoMipmapsWidth[0].size()+1 : m_isoMipmaps.size();
+    //+1 because the first mipmap is the full image, which is stored in m_isoMipmaps[0] but not in m_anisoMipmapsWidth
+}
+
+template <class I>
+size_t Mipmap<I>::numberMipmapsHeight() const
+{
+    if(!m_generated)
+        return 0;
+    return m_mode == MIPMAP_ANISO ? m_anisoMipmapsHeight[0].size()+1 : m_isoMipmaps.size();
+}
+
+template <class I>
+const I& Mipmap<I>::mipmap(unsigned xPowReduction, unsigned yPowReduction) const
+{
+    assert(m_generated && "Mipmap::mipmap: mipmap has not been yet generated (use function generate() or provide necessary arguments to constructor)");
     assert((m_mode!=MIPMAP_ISO || xPowReduction==yPowReduction) && "Mipmap::mipmap: xReduction and yReduction must be identical when using isotropic filtering");
-    xPowReduction = std::min(xPowReduction, (int)m_isoMipmaps.size()-1);
-    yPowReduction = std::min(yPowReduction, (int)m_isoMipmaps.size()-1);
+    xPowReduction = std::min(xPowReduction, (unsigned)m_isoMipmaps.size()-1);
+    yPowReduction = std::min(yPowReduction, (unsigned)m_isoMipmaps.size()-1);
 
     if(xPowReduction==yPowReduction)
     {
@@ -258,7 +298,7 @@ class MipmapBooleanImage : public Mipmap< ImageGrayb >
 {
 public:
 
-    MipmapBooleanImage(const ImageGrayb& texture, mipmap_mode_t mode=MIPMAP_ISO, int maxReductionLevel=0);
+    MipmapBooleanImage(const ImageGrayb& texture, mipmap_mode_t mode=MIPMAP_ISO, unsigned maxReductionLevel=0);
 
 protected:
 
@@ -267,7 +307,7 @@ protected:
     void filterDivide2Full(const ImageGrayb& texture, ImageGrayb& result);
 };
 
-MipmapBooleanImage::MipmapBooleanImage(const ImageGrayb& texture, mipmap_mode_t mode, int maxReductionLevel):
+MipmapBooleanImage::MipmapBooleanImage(const ImageGrayb& texture, mipmap_mode_t mode, unsigned maxReductionLevel):
     Mipmap<ImageGrayb>(texture, mode, maxReductionLevel)
 {}
 
@@ -300,6 +340,221 @@ void MipmapBooleanImage::filterDivide2Full(const ImageGrayb& texture, ImageGrayb
             || texture.pixelAbsolute(2*x, 2*y+1)
             || texture.pixelAbsolute(2*x+1, 2*y+1);
     });
+}
+
+template<typename I>
+class MipmapBitmask : public Mipmap<I>
+{
+public:
+
+    MipmapBitmask(const I& texture, typename Mipmap<I>::mipmap_mode_t mode=Mipmap<I>::MIPMAP_ISO, unsigned maxReductionLevel=0);
+
+protected:
+
+    void filterDivide2Width(const I& texture, I& result);
+    void filterDivide2Height(const I& texture, I& result);
+    void filterDivide2Full(const I& texture, I& result);
+};
+
+template<typename I>
+MipmapBitmask<I>::MipmapBitmask(const I& texture, typename Mipmap<I>::mipmap_mode_t mode, unsigned maxReductionLevel) :
+    Mipmap<I>(texture, mode, maxReductionLevel)
+{}
+
+template<typename I>
+void MipmapBitmask<I>::filterDivide2Width(const I& texture, I& result)
+{
+    result.initItk(texture.width()/2, texture.height());
+    result.for_all_pixels([&] (bool &pix, int x, int y)
+    {
+        pix = texture.pixelAbsolute(2*x, y) | texture.pixelAbsolute(2*x+1, y);
+    });
+}
+
+template<typename I>
+void MipmapBitmask<I>::filterDivide2Height(const I& texture, I& result)
+{
+    result.initItk(texture.width(), texture.height()/2);
+    result.for_all_pixels([&] (bool &pix, int x, int y)
+    {
+        pix = texture.pixelAbsolute(x, 2*y) | texture.pixelAbsolute(x, 2*y+1);
+    });
+}
+
+template<typename I>
+void MipmapBitmask<I>::filterDivide2Full(const I& texture, I& result)
+{
+    result.initItk(texture.width()/2, texture.height()/2);
+    result.for_all_pixels([&] (bool &pix, int x, int y)
+    {
+        pix = texture.pixelAbsolute(2*x, 2*y)
+            | texture.pixelAbsolute(2*x+1, 2*y)
+            | texture.pixelAbsolute(2*x, 2*y+1)
+            | texture.pixelAbsolute(2*x+1, 2*y+1);
+    });
+}
+
+template<typename I>
+/**
+ * @brief The MipmapContentExchange class is a particular type of mipmap
+ * which embodies mipmapped portions of an image.
+ * These portions must have been stored in an image and mipmapped before using this class.
+ * Content must have been shifted and emplaced where the patch's default content is
+ * in order to fit the size.
+ */
+class MipmapCEContent : public Mipmap<I>
+{
+public:
+    /**
+     * @brief MipmapCEContent constructor for MipmapCEContent.
+     * Immediately produces the output image.
+     * @param contentColor input-sized mipmap containing the content, shifted to the position of the patch.
+     * @param patchAlpha input-sized mipmap containing the alpha information of the patch.
+     */
+    MipmapCEContent(const Mipmap<I> &contentColor, const Mipmap<ImageGrayd> &patchAlpha);
+
+    void generate(const I& texture, typename Mipmap<I>::mipmap_mode_t mode=Mipmap<I>::MIPMAP_ISO, unsigned maxPowReductionLevel=0);
+};
+
+template<typename I>
+MipmapCEContent<I>::MipmapCEContent(const Mipmap<I> &contentColor, const Mipmap<ImageGrayd> &patchAlpha) :
+    Mipmap<I>()
+{
+    assert(contentColor.mode()==patchAlpha.mode() && contentColor.mipmap(0, 0).size()==patchAlpha.mipmap(0, 0).size()
+           && contentColor.numberMipmapsWidth() == patchAlpha.numberMipmapsWidth()
+           && contentColor.numberMipmapsHeight() == patchAlpha.numberMipmapsHeight()
+           && "MipmapCEContent(): contentColor mipmaps and patchAlpha mipmaps don't seem to come from the same process (different mipmap modes or differents sizes)");
+    this->m_mode=contentColor.mode();
+    this->m_generated=true;
+
+    unsigned i, j, maxIterations;
+    unsigned xMin, yMin, xMax, yMax;
+    auto emplaceMipmap = [&] ()
+    {
+        const I& mipmapContentColor=contentColor.mipmap(i, j);
+        const ImageGrayd &mipmapPatchAlpha=patchAlpha.mipmap(i, j);
+        I* mipmapContentBox=NULL;
+
+        //slighty awkward assignement, corresponds to mipmap(i, j)
+        if(i == j) //isotropic
+            mipmapContentBox = &this->m_isoMipmaps[i];
+        else
+            mipmapContentBox = i > j ? &this->m_anisoMipmapsWidth[j][i-j-1] : &this->m_anisoMipmapsHeight[i][j-i-1];
+
+        //first iteration: find the edges of the box using the patch's alpha.
+        //note how patches can bleed from one edge of the image to another due to periodicity.
+
+        xMin=mipmapPatchAlpha.width()-1;
+        yMin=mipmapPatchAlpha.height()-1;
+        xMax=0;
+        yMax=0;
+        mipmapPatchAlpha.for_all_pixels([&] (typename ImageGrayd::PixelType &pix, int x, int y)
+        {
+            if(pix>0) //meaning there's a patch portion
+            {
+                xMin=std::min(xMin, (unsigned)x);
+                yMin=std::min(yMin, (unsigned)y);
+                xMax=std::max(xMax, (unsigned)x);
+                yMax=std::max(xMax, (unsigned)y);
+            }
+        });
+
+        //we have found a non periodic bounding box.
+        //Now we need to check if the bounding box is aperiodic in width.
+        //TODO: if a bug is found, it could come from this part. It's easy to make a mistake.
+        //TODO: this part of the code WILL also be problematic if some periodic boxes take the entire width.
+        if(xMin == 0 && xMax == mipmapPatchAlpha.width()-1)
+        { //this block occurs when the bounding box is suspected to be periodic in width.
+            //check discontinuities in image width (an empty column)
+            int oldXMax = xMax;
+            for(int x=xMin; x<=xMax; ++x)
+            {
+                int y;
+                for(y=yMin; y<=yMax && mipmapPatchAlpha.pixelAbsolute(x, y)==0; ++y);
+                if(y>yMax) //discontinuity found
+                    xMax=x-1; //assign xMax to the last full row we found, breaking the loop
+            }
+            if(oldXMax != xMax) //if false, it means the content was simply taking the entire width
+            {
+                //iterate the other way until a discontinuity is found
+                for(int x=oldXMax; x>=xMin; --x)
+                {
+                    int y;
+                    for(y=yMin; y<=yMax && mipmapPatchAlpha.pixelAbsolute(x, y)==0; ++y);
+                    if(y>yMax) //discontinuity found
+                        xMin=x+1; //assign xMin to the last full row we found, breaking the loop
+                }
+            }
+        }
+        //from here onwards, the bounding box shall take periodicity in width in account.
+        //now for the periodicity in height, it's the same iteration, but in height, with y instead of x, and a modulo on x.
+        if(yMin == 0 && yMax == mipmapPatchAlpha.height()-1)
+        {
+            int oldYMax = yMax;
+            //notice the new iterator: we use != to xMax+1 and the iteration is periodic on the width.
+            for(int y=yMin; y<=yMax; ++y)
+            {
+                int x;
+                for(x=xMin; x!=xMax+1 && mipmapPatchAlpha.pixelAbsolute(x, y)==0; x = (x+1)%mipmapPatchAlpha.width());
+                if(x==xMax+1) //discontinuity found (don't use >)
+                    yMax=y-1;
+            }
+            if(oldYMax != yMax)
+            {
+                for(int y=oldYMax; y>=yMin; --y)
+                {
+                    int x;
+                    for(x=xMin; x!=xMax+1 && mipmapPatchAlpha.pixelAbsolute(x, y)==0; x = (x+1)%mipmapPatchAlpha.width());
+                    if(x==xMax+1)
+                        yMin=y+1;
+                }
+            }
+        }
+        //from here onwards we have found the correctly periodic bounding box.
+        //TODO: this code should be copied, or even exported, to the place we compute the boxes for the patches.
+        //Then, we could simply pass every boxes we computed to this function.
+
+        //Allocate the new mipmap
+
+        mipmapContentBox->initItk(xMin<xMax ? xMax-xMin+1 : mipmapPatchAlpha.width()-xMin + xMax,
+                                  yMin<yMax ? yMax-yMin+1 : mipmapPatchAlpha.height()-yMin + yMax, true);
+        //Fill up the new mipmap
+        mipmapContentBox->for_all_pixels([&] (typename I::PixelType &pix, int x, int y)
+        {
+            pix = mipmapContentColor.pixelAbsolute((xMin+x)%mipmapContentColor.width(),
+                                                   (yMin+y)%mipmapContentColor.height());
+        });
+    };
+
+    //allocation
+
+    maxIterations = std::max(contentColor.numberMipmapsWidth(), contentColor.numberMipmapsHeight());
+    this->m_isoMipmaps.resize(maxIterations);
+    if(contentColor.mode() == Mipmap<I>::MIPMAP_ANISO)
+    {
+        this->m_anisoMipmapsWidth.resize(contentColor.numberMipmapsWidth()-1);
+        this->m_anisoMipmapsHeight.resize(contentColor.numberMipmapsHeight()-1);
+    }
+
+    if(contentColor.mode()==Mipmap<I>::MIPMAP_ISO)
+    for(i=0, j=0; i<maxIterations; ++i, ++j)
+        emplaceMipmap();
+
+    else if(contentColor.mode()==Mipmap<I>::MIPMAP_ANISO)
+        for(i=0; i<contentColor.numberMipmapsWidth(); ++i)
+            for(j=0; j<contentColor.numberMipmapsHeight(); ++i)
+                emplaceMipmap();
+
+
+}
+
+template<typename I>
+void MipmapCEContent<I>::generate(const I& texture, typename Mipmap<I>::mipmap_mode_t mode, unsigned maxPowReductionLevel)
+{
+    (void) texture;
+    (void) mode;
+    (void) maxPowReductionLevel;
+    return;
 }
 
 #endif //__MIPMAP__
