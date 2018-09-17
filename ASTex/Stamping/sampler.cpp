508def60c371b7207bfb7e20dedb7ecc4ea61a43
Nicolas Lutz <nicolas.lutz@icube.unistra.fr>
2018-09-17 17:35:24 +0200

    Quick-fixed Poisson Sampler.
    Instead of yielding the wrong number of points,
    it now yields points that are sometimes too close to each others.

--- ASTex/Stamping/sampler.cpp
+++ ASTex/Stamping/sampler.cpp
@@ -47,10 +47,10 @@ std::vector<Eigen::Vector2f> SamplerUniform::generate()
 
 std::vector<Eigen::Vector2f> SamplerPoisson::generate()
 {
-    double minDistance=m_minDistance;
+    float minDistance=m_minDistance;
     if ( minDistance < 0.0f )
     {
-        minDistance = sqrt( float(m_nbPoints) ) / float(m_nbPoints);
+        minDistance = sqrt( float(m_nbPoints) ) / m_nbPoints;
     }
 
     std::vector<Eigen::Vector2f> SamplePoints;
@@ -64,45 +64,39 @@ std::vector<Eigen::Vector2f> SamplerPoisson::generate()
 
     sGrid Grid( GridW, GridH, CellSize );
 
-    Eigen::Vector2f FirstPoint;
-    do {
-        FirstPoint = Eigen::Vector2f( m_generator.RandomFloat(), m_generator.RandomFloat() );
-    } while (!(m_generateInCircle ? IsInCircle(FirstPoint) : IsInRectangle(FirstPoint)));
-
-    // update containers
-    ProcessList.push_back( FirstPoint );
-    SamplePoints.push_back( FirstPoint );
-    Grid.Insert( FirstPoint );
-
-    // generate new points for each point in the queue
-    while ( !ProcessList.empty() && SamplePoints.size() < m_nbPoints )
+    while(SamplePoints.size()<m_nbPoints)
     {
-#if POISSON_PROGRESS_INDICATOR
-        // a progress indicator, kind of
-        if ( SamplePoints.size() % 100 == 0 ) std::cout << ".";
-#endif // POISSON_PROGRESS_INDICATOR
+        Eigen::Vector2f FirstPoint;
+        do {
+            FirstPoint = Eigen::Vector2f( m_generator.RandomFloat(), m_generator.RandomFloat() );
+        } while (!(m_generateInCircle ? IsInCircle(FirstPoint) : IsInRectangle(FirstPoint)));
 
-        Eigen::Vector2f Point = PopRandom( ProcessList, m_generator );
+        // update containers
+        ProcessList.push_back( FirstPoint );
+        SamplePoints.push_back( FirstPoint );
+        Grid.Insert( FirstPoint );
 
-        for ( int i = 0; i < m_newPointsCount; i++ )
+        // generate new points for each point in the queue
+        while ( !ProcessList.empty() && SamplePoints.size() < m_nbPoints )
         {
-            Eigen::Vector2f NewPoint = GenerateRandomPointAround( Point, minDistance, m_generator );
+            Eigen::Vector2f Point = PopRandom( ProcessList, m_generator );
 
-            bool Fits = m_generateInCircle ? IsInCircle(NewPoint) : IsInRectangle(NewPoint);
-
-            if ( Fits && !Grid.IsInNeighbourhood( NewPoint, minDistance, CellSize ) )
+            for ( int i = 0; i < m_newPointsCount; i++ )
             {
-                ProcessList.push_back( NewPoint );
-                SamplePoints.push_back( NewPoint );
-                Grid.Insert( NewPoint );
-                continue;
+                Eigen::Vector2f NewPoint = GenerateRandomPointAround( Point, minDistance, m_generator );
+
+                bool Fits = m_generateInCircle ? IsInCircle(NewPoint) : IsInRectangle(NewPoint);
+
+                if ( Fits && !Grid.IsInNeighbourhood( NewPoint, minDistance, CellSize ) )
+                {
+                    ProcessList.push_back( NewPoint );
+                    SamplePoints.push_back( NewPoint );
+                    Grid.Insert( NewPoint );
+                    continue;
+                }
             }
         }
     }
-
-#if POISSON_PROGRESS_INDICATOR
-    std::cout << std::endl << std::endl;
-#endif // POISSON_PROGRESS_INDICATOR
 
     return SamplePoints;
 }
