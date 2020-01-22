12ddb92be892d1240d880f81c0764126b559cdda
Nathaniel Seyler <nathaniel.seyler@etu.unistra.fr>
2020-01-22 10:39:33 +0100

    fenetre d'observation plus proche [-1,1]

--- Tuto/tuto_noise_filter.cpp
+++ Tuto/tuto_noise_filter.cpp
@@ -1,4 +1,3 @@
-#include <ASTex/image_gray.h>
 #include <ASTex/noise.h>
 #include <ASTex/easy_io.h>
 
@@ -10,9 +9,13 @@ int main()
     ImageGray<T> im(512,512);
     Noise<T> noise;
 
-    im.parallel_for_all_pixels([&](T &p,int x,int y)
+    im.parallel_for_all_pixels([&](T &p,int i,int j)
     {
-        T r = noise.basic2D(T(x),T(y)); // noise value at (x,y)
+        T born_sup(2);
+        T center = born_sup / T(2);
+        T x = T(i) / T(im.width()) * born_sup - center;     // x between [-center,center]
+        T y = T(j) / T(im.height()) * born_sup - center;    // y between [-center,center]
+        T r = noise.basic2D(x,y);                           // noise value at (x,y)
 
         p = r;
     });
