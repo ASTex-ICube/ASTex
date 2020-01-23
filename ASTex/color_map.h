0870fb82e7b82800876f2d0b1e465a0b7cb389cc
Nathaniel Seyler <nathaniel.seyler@etu.unistra.fr>
2020-01-23 17:50:12 +0100

    add color_map

---
+++ ASTex/color_map.h
@@ -0,0 +1,52 @@
+#ifndef COLOR_MAP_H
+#define COLOR_MAP_H
+
+#include <ASTex/image_rgb.h>
+#include <map>
+
+namespace ASTex {
+
+
+template <typename T>
+class Color_map
+{
+public :
+    using Color = Eigen::Matrix<T,3,1>;
+private:
+    std::map<int, Color > palette;
+public:
+    Color_map() {}
+    void add_color(const int &pos,const Color &col) {
+        palette.insert(std::pair<int, Color>(pos,col));
+    }
+
+    typename ImageRGB<T>::PixelType map(const T &x)
+    {
+        T pas = T(1) / T(palette.rbegin()->first);
+
+        for(auto it = palette.begin();it!=palette.end();++it)
+        {
+            T inf = T(it->first) * pas;
+            T sup = T((++it)->first) * pas;
+            it--;
+            if (x >= inf && x <= sup) {
+                T t = (sup - inf ) * x + inf;
+                Color c_inf = it->second;
+                Color c_sup = (++it)->second;
+                //it--;
+                return ImageRGB<T>::itkPixel((1 - t) * c_inf + t * c_sup);
+            }
+        }
+
+        return ImageRGB<T>::itkPixel(0);
+    }
+
+    void export_palette()
+    {
+
+    }
+};
+
+}
+
+#endif // COLOR_MAP_H
--- Tuto/tuto_noise_filter.cpp
+++ Tuto/tuto_noise_filter.cpp
@@ -1,5 +1,6 @@
 #include <ASTex/noise.h>
 #include <ASTex/easy_io.h>
+#include <ASTex/color_map.h>
 #include <Eigen/Eigen>
 
 using namespace ASTex;
@@ -9,10 +10,16 @@ int main()
     using T = double;
     using Vec2 = Eigen::Matrix<T,2,1>;
     using Mat22 = Eigen::Matrix<T,2,2>;
-    ImageGray<T> im(512,512);
+    ImageRGB<T> im(512,512);
     Noise<T> noise;
+    Color_map<T> cm;
+    cm.add_color(0,Color_map<T>::Color(0,0,0));
+    cm.add_color(1,Color_map<T>::Color(0,0,1));
+    cm.add_color(3,Color_map<T>::Color(0,1,0));
+    cm.add_color(4,Color_map<T>::Color(1,0,0));
+    cm.add_color(6,Color_map<T>::Color(1,1,1));
 
-    im.parallel_for_all_pixels([&](T &p,int i,int j)
+    im.for_all_pixels([&](ImageRGB<T>::PixelType &p,int i,int j)
     {
         Mat22 borns;
         borns << 2, 0, 0, 2;
@@ -20,8 +27,8 @@ int main()
         Vec2 vec =  borns * Vec2(T(i) / T(im.width()), T(j) / T(im.height())) - center;
 //        T x = T(i) / T(im.width()) * borns(0) - center(0);      // x between [-center,center]
 //        T y = T(j) / T(im.height()) * borns(3) - center(1);     // y between [-center,center]
-        T r = noise.basic2D(vec(0),vec(1));                               // noise value at (x,y)
-        p = r;
+        T r = noise.basic2D(vec(0),vec(1));                               // noise value at (x,y), vec(0) =x et vec(1)=y
+        p = cm.map(r);
     });
 
     IO::save01_in_u8(im,TEMPO_PATH + "noise.png");
