6f776abef658ac53217805456ed69047490f9e96
Nathaniel Seyler <nathaniel.seyler@etu.unistra.fr>
2020-01-24 17:00:30 +0100

    add de f et de f2 et de leurs mipmap

--- Tuto/tuto_noise_filter.cpp
+++ Tuto/tuto_noise_filter.cpp
@@ -2,6 +2,7 @@
 #include <ASTex/easy_io.h>
 #include <ASTex/color_map.h>
 #include <Eigen/Eigen>
+#include <ASTex/mipmap.h>
 
 using namespace ASTex;
 
@@ -10,7 +11,11 @@ int main()
     using T = double;
     using Vec2 = Eigen::Matrix<T,2,1>;
     using Mat22 = Eigen::Matrix<T,2,2>;
-    ImageRGB<T> im(512,512);
+    ImageGray<T> im_f(512,512);
+    ImageGray<T> im_f2(512,512);
+
+    ImageRGB<T> filtered(512,512);
+
     Noise<T> noise;
     Color_map<T> cm;
     cm.add_color(0,Color_map<T>::Color(0,0,0));
@@ -20,23 +25,56 @@ int main()
     cm.add_color(6,Color_map<T>::Color(1,1,1));
 
     cm.export_palette(TEMPO_PATH + "palette.gnu");
+    cm.export_courbe(TEMPO_PATH + "data.txt");
 
-    im.parallel_for_all_pixels([&](ImageRGB<T>::PixelType &p,int i,int j)
+
+    ImageRGB<T> c0_(512,512);
+    T sigma_max(6);
+    c0_.parallel_for_all_pixels([&](ImageRGB<T>::PixelType &p,int i,int j)
     {
-        Mat22 borns;
-        borns << 2, 0, 0, 2;
-        Vec2 center = Vec2(borns(0),borns(3)) / T(2);
+        T f = j / c0_.height();
+        T sigma = i / c0_.width() * sigma_max;
 
+
+    });
+
+    Mat22 borns;
+    borns << 2, 0, 0, 2;
+    Vec2 center = Vec2(borns(0),borns(3)) / T(2);
+
+    im_f.parallel_for_all_pixels([&](ImageGray<T>::PixelType &p,int i,int j)
+    {
         // x = vec(0) between [-center(0),center(0)]
         // y = vec(1) between [-center(1),center(1)]
-        Vec2 vec =  borns * Vec2(T(i) / T(im.width()), T(j) / T(im.height())) - center;
+        Vec2 vec =  borns * Vec2(T(i) / T(im_f.width()), T(j) / T(im_f.height())) - center;
 
         // noise value at (x,y), vec(0) =x et vec(1)=y
         T r = noise.basic2D(vec(0),vec(1));
-        p = cm.map(r);
+        p = r;
+        im_f2.pixelAbsolute(i,j) = r * r;
     });
 
-    IO::save01_in_u8(im,TEMPO_PATH + "noise.png");
+    Mipmap<ImageGray<T>> m(im_f);
+    Mipmap<ImageGray<T>> m2(im_f2);
+
+    m.generate();
+    m2.generate();
+
+    filtered.parallel_for_all_pixels([&] (ImageRGB<T>::PixelType &p, int i, int j)
+    {
+        p = cm.map(im_f.pixelAbsolute(i,j));
+    });
+
+    ImageGray<T> fmp;
+    m.fullMipmap(fmp);
+
+    ImageGray<T> fmp2;
+    m2.fullMipmap(fmp2);
+
+
+    IO::save01_in_u8(fmp,TEMPO_PATH + "noise.png");
+    IO::save01_in_u8(fmp2,TEMPO_PATH + "noise2.png");
+    IO::save01_in_u8(filtered,TEMPO_PATH + "noise_filtered.png");
 
     return 0;
 }
