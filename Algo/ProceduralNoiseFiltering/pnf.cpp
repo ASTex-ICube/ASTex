3491efa9c9f800b5e6cd089163c718e4579d5b2b
Nathaniel Seyler <nathaniel.seyler@etu.unistra.fr>
2020-02-11 17:42:29 +0100

    update

--- Algo/ProceduralNoiseFiltering/pnf.cpp
+++ Algo/ProceduralNoiseFiltering/pnf.cpp
@@ -5,27 +5,17 @@ using namespace ASTex;
 
 int main()
 {
-    Noise<T> noise(1000,10.,20.);
+    Noise<T> noise(32,10.,20.);
     Color_map<T> cm;
 
-//  palette 1
-    cm.add_color(0,Color(1,1,0));
-    cm.add_color(40,Color(1,0,0));
-    cm.add_color(59,Color(0,0,0));
-    cm.add_color(60,Color(1,1,1));
-    cm.add_color(100,Color(1,1,1));
+//  palette a
+//    cm.add_color(0,Color(1,1,0));
+//    cm.add_color(40,Color(1,0,0));
+//    cm.add_color(59,Color(0,0,0));
+//    cm.add_color(60,Color(1,1,1));
+//    cm.add_color(100,Color(1,1,1));
 
-
-//  palette 2
-//    cm.add_color(0,Color(0,0,1));
-//    cm.add_color(1,Color(0,1,0));
-//    cm.add_color(2,Color(1,0,0));
-
-//  palette 3
-//    cm.add_color(0, Color(154./255., 173./255., 213./255.));
-//    cm.add_color(1, Color(196./255., 192./255., 144./255.));
-
-//  palette 4
+//  palette b
 //    cm.add_color(0, Color(0,0,0));
 //    cm.add_color(4, Color(0,0,0));
 //    cm.add_color(5, Color(1,1,1));
@@ -33,14 +23,23 @@ int main()
 //    cm.add_color(9, Color(199./255., 139./255., 105./255.));
 //    cm.add_color(10, Color(199./255., 139./255., 105./255.));
 
+//  palette c
+    cm.add_color(0, Color(0., 0., 1.));
+    cm.add_color(1, Color(1., 0., 0.));
+
+//  palette d
+//    cm.add_color(0,Color(0,0,1));
+//    cm.add_color(1,Color(0,1,0));
+//    cm.add_color(2,Color(1,0,0));
+
     cm.export_courbe(TEMPO_PATH + "data.txt");
-    cm.export_img_palette(512, TEMPO_PATH + "palette.png");
+//    cm.export_img_palette(512, TEMPO_PATH + "palette.png");
 
 
     //filtrage color map
     auto start_chrono = std::chrono::system_clock::now();
 
-    cm.filter(512,512,200,0.5);
+    cm.filter(512,512,200,0.3);
 
     std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start_chrono;
     std::cout << "filtrage de la color map timing: " << elapsed_seconds.count() << " s." << std::endl;
@@ -52,7 +51,7 @@ int main()
 //    IO::loadu8_in_01(c0_,TEMPO_PATH+ "color_map_filtered.png");
 //    cm.set_filtered(c0_,T(0.5));
 
-    Vec2 w_size(32,32);
+    Vec2 w_size(2,2);
     Vec2 im_size(512,512);
 
     //unfilterd noise
--- Algo/ProceduralNoiseFiltering/pnf_texture.cpp
+++ Algo/ProceduralNoiseFiltering/pnf_texture.cpp
@@ -7,11 +7,11 @@ using namespace ASTex;
 
 int main()
 {
-//    ImageGray<T> noise;
-//    IO::loadu16_in_01(noise, TEMPO_PATH + "noise/gabor_aniso_repeat_gauss.png");
+    ImageGray<T> noise;
+    IO::loadu16_in_01(noise, TEMPO_PATH + "noise/voronoi_repeat_non_gauss.png");
 
-    ImageSpectrald psd;
-    IO::loadu8_in_01(psd, TEMPO_PATH + "spectra/spectrum_9.png");
+//    ImageSpectrald psd;
+//    IO::loadu8_in_01(psd, TEMPO_PATH + "spectra/donut_black.png");
 
 //    ImageGray<T> example_noise;
 //    IO::loadu8_in_01(example_noise, TEMPO_PATH + "gray_png/gc12.png");
@@ -19,21 +19,21 @@ int main()
 //    ImageSpectrald psd, phase;
 //    Fourier::fftForwardModulusAndPhase(example_noise, psd, phase);
 
-    TextureNoise<T> texture_noise(psd, 0.5, 0.1667);
-//    texture_noise.setNoise(noise);
+    TextureNoise<T> texture_noise;
+    texture_noise.setNoise(noise);
     Color_map<T> cm;
 
     ImageRGB<T> c0_;
     IO::loadu8_in_01(c0_,TEMPO_PATH+ "color_map_filtered.png");
-    cm.set_filtered(c0_,0.5);
+    cm.set_filtered(c0_,0.3);
 
-    Vec2 w_size(512,512);
-    Vec2 im_size(512,512);
+    Vec2 w_size(noise.width()*0.125, noise.height()*0.125);
+    Vec2 im_size(1024,1024);
 
 
     // compute
 
-    ImageGray<T> noise = texture_noise.getNoise();
+//    ImageGray<T> noise = texture_noise.getNoise();
 
 
     // noise unfiltered
@@ -70,17 +70,17 @@ int main()
     elapsed_seconds = std::chrono::system_clock::now() - start_chrono;
     std::cout << "synthe good filtering timing: " << elapsed_seconds.count() << " s." << std::endl;
 
-    IO::save01_in_u8(noise, TEMPO_PATH + "texture_noise.png");
-    IO::save01_in_u8(im_noise_cm,TEMPO_PATH + "texture_noise_unfilered.png");
-    IO::save01_in_u8(im_ground_truth,TEMPO_PATH + "texture_ground_truth.png");
-    IO::save01_in_u8(im_noise_cm_naive_filter,TEMPO_PATH + "texture_noise_naive_filtering.png");
-    IO::save01_in_u8(im_noise_cm_good_filter,TEMPO_PATH + "texture_noise_goood_filtering.png");
+//    IO::save01_in_u8(noise, TEMPO_PATH + "texture_noise.png");
+//    IO::save01_in_u8(im_noise_cm,TEMPO_PATH + "texture_noise_unfilered.png");
+//    IO::save01_in_u8(im_ground_truth,TEMPO_PATH + "texture_ground_truth.png");
+//    IO::save01_in_u8(im_noise_cm_naive_filter,TEMPO_PATH + "texture_noise_naive_filtering.png");
+//    IO::save01_in_u8(im_noise_cm_good_filter,TEMPO_PATH + "texture_noise_goood_filtering.png");
 
-//    IO::save01_in_u16(noise, TEMPO_PATH + "texture_noise.png");
-//    IO::save01_in_u16(im_noise_cm,TEMPO_PATH + "texture_noise_unfilered.png");
-//    IO::save01_in_u16(im_ground_truth,TEMPO_PATH + "texture_ground_truth.png");
-//    IO::save01_in_u16(im_noise_cm_naive_filter,TEMPO_PATH + "texture_noise_naive_filtering.png");
-//    IO::save01_in_u16(im_noise_cm_good_filter,TEMPO_PATH + "texture_noise_goood_filtering.png");
+//    IO::save01_in_u16(noise, TEMPO_PATH + "texture2_noise.png");
+    IO::save01_in_u16(im_noise_cm,TEMPO_PATH + "texture2_noise_unfilered.png");
+    IO::save01_in_u16(im_ground_truth,TEMPO_PATH + "texture2_ground_truth.png");
+    IO::save01_in_u16(im_noise_cm_naive_filter,TEMPO_PATH + "texture2_noise_naive_filtering.png");
+    IO::save01_in_u16(im_noise_cm_good_filter,TEMPO_PATH + "texture2_noise_goood_filtering.png");
 
     return 0;
 }
