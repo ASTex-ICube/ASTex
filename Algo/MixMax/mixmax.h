479e97f5be14377111ff8a153d58a6705043bad5
Romain Fournier <romain.fournier.095@gmail.com>
2023-11-24 14:15:03 +0100

    cleanup

--- Algo/MixMax/mixmax.h
+++ Algo/MixMax/mixmax.h
@@ -6,72 +6,137 @@
 
 using namespace ASTex;
 
-class MixMax {
-    public: MixMax() {}
+namespace MixMax {
+    /*
+    * PRIVATE FUNCTIONS
+    */
+    namespace {
+        double phi (double x) {
+            return 0.5 * (1.0 + erf(x / std::sqrt(2.0)));
+        }
 
-    private: double Phi (double x) {
-        return 0.5 * (1.0 + erf(x / std::sqrt(2.0)));
+        itk::RGBPixel<double> get_mean (ImageRGBd *T, Region& r) {
+            itk::RGBPixel<double> mean;
+            mean.SetRed(0);
+            mean.SetGreen(0);
+            mean.SetBlue(0);
+            double n = 0;
+
+            T->for_region_pixels(r, [&] (itk::RGBPixel<double>& p) {
+                mean += p;
+                n += 1;
+            });
+            mean = mean * (1.0 / n);
+
+            return mean;
+        }
+
+        itk::RGBPixel<double> get_mean (ImageRGBd *T) {
+            Region r = gen_region(0, 0, T->width(), T->height());
+            return get_mean(T, r);
+        }
+
+        double get_mean (ImageGrayd *T, Region r) {
+            double mean = 0.0;
+            double n = 0;
+
+            T->for_region_pixels(r, [&] (double& p) {
+                mean += p;
+                n += 1;
+            });
+            mean = mean / n;
+
+            return mean;
+        }
+
+        double get_mean (ImageGrayd *T) {
+            Region r = gen_region(0, 0, T->width(), T->height());
+            return get_mean(T, r);
+        }
+
+        double get_variance (ImageGrayd *T, Region r) {
+            double mean = get_mean(T, r);
+            double variance = 0;
+            double n = 0;
+
+            T->for_region_pixels(r, [&] (double& p) {
+                variance += (p - mean) * (p - mean);
+                n += 1;
+            });
+            variance = variance * (1.0 / n);
+
+            return variance;
+        }
+
+        double get_variance (ImageGrayd *T) {
+            Region r = gen_region(0, 0, T->width(), T->height());
+
+            return get_variance(T, r);
+        }
+
+        ImageGrayd* center (ImageGrayd *T) {
+            ImageGrayd *out = new ImageGrayd(T->width(), T->height(), false);
+            float mean = get_mean(T);
+
+            out->for_all_pixels([&] (double& p, int x, int y) {
+                p = T->pixelAbsolute(x, y) - mean;
+            });
+
+            return out;
+        }
     }
-
-    private: double get_mean (ImageGrayd *T) {
-        double mean = 0.0;
-        double n = 0;
-
-        T->for_all_pixels([&] (double& p) {
-            mean += p;
-            n += 1;
-        });
-
-        mean = mean * (1.0 / n);
-
-        return mean;
-    }
-
-    private: ImageGrayd* center (ImageGrayd *T) {
-        ImageGrayd *out = new ImageGrayd(T->width(), T->height(), false);
-
-        float mean = get_mean(T);
-
-        out->for_all_pixels([&] (double& p, int x, int y) {
-            p = T->pixelAbsolute(x, y) - mean;
-        });
-
-        return out;
-    }
-
-    public: ImageRGBd* blend (ImageRGBd *T1, ImageRGBd *T2, ImageGrayd *S1, ImageGrayd *S2, ImageGrayd *V, double lambda = 0, double interpolation_field_multiplier = 1.0) {
-        ImageRGBd *out = new ImageRGBd(T1->width(), T1->height(), false);
-
+    
+    /*
+    * MIXMAX IMPLEMENTATION
+    */
+    ImageRGBd* blend (ImageRGBd *T1, ImageRGBd *T2, ImageGrayd *S1, ImageGrayd *S2, ImageGrayd *V, int mip_level = 0, double lambda = 0, double interpolation_field_multiplier = 1.0) {
+        int region_size = std::pow(2, mip_level);
+        ImageRGBd *out = new ImageRGBd(T1->width() / region_size, T1->height() / region_size, false);
         ImageGrayd* S1c = center(S1);
         ImageGrayd* S2c = center(S2);
 
         out->parallel_for_all_pixels([&] (itk::RGBPixel<double>& p, int x, int y) {
-            itk::RGBPixel<double> t1 = T1->pixelAbsolute(x, y);
-            itk::RGBPixel<double> t2 = T2->pixelAbsolute(x, y);
-            double s1 = S1c->pixelAbsolute(x, y);
-            double s2 = S2c->pixelAbsolute(x, y);
-            double v1 = V->pixelAbsolute(x, y);
-            double v2 = 1.0 - v1;
+            Region r = gen_region(x * region_size, y * region_size, region_size, region_size);
+            itk::RGBPixel<double> t1_mean = get_mean(T1, r);
+            itk::RGBPixel<double> t2_mean = get_mean(T2, r);
+            double s1_mean = get_mean(S1c, r);
+            double s2_mean = get_mean(S2c, r);
+            double s1_variance = get_variance(S1c, r);
+            double s2_variance = get_variance(S2c, r);
+            double v1_mean = get_mean(V, r);
+            double v2_mean = 1.0 - v1_mean;
 
-            v1 *= interpolation_field_multiplier;
-            v2 *= interpolation_field_multiplier;
-
-            double mu = (s2 + v2) - (s1 + v1);
-            double sigma = sqrt(2.0 * pow(lambda, 2));
-
+            v1_mean *= interpolation_field_multiplier;
+            v2_mean *= interpolation_field_multiplier;
+            double mean = (s2_mean + v2_mean) - (s1_mean + v1_mean);
+            double std_dev = sqrt(s1_variance + s2_variance + 2.0 * pow(lambda, 2));
             double w1 = 0;
-            if (sigma < 0.001) {
-                if (mu < 0) w1 = 1;
+            if (std_dev < 0.001) {
+                if (mean < 0) w1 = 1;
             } else {
-                w1 = 1.0 - Phi(mu / sigma);
+                w1 = 1.0 - phi(mean / std_dev);
             }
             double w2 = 1.0 - w1;
-            
-            p = t1 * w1 + t2 * w2;
-        });
 
+            p = t1_mean * w1 + t2_mean * w2;
+        });
         delete S1c;
         delete S2c;
+
+        return out;
+    }
+
+    ImageRGBd* ground_truth (ImageRGBd *T1, ImageRGBd *T2, ImageGrayd *S1, ImageGrayd *S2, ImageGrayd *V, int mip_level = 0, double lambda = 0, double interpolation_field_multiplier = 1.0) {
+        int region_size = std::pow(2, mip_level);
+        ImageRGBd *out = new ImageRGBd(T1->width() / region_size, T1->height() / region_size, false);
+        ImageRGBd *mixmax = blend(T1, T2, S1, S2, V, 0, lambda, interpolation_field_multiplier);
+
+        out->parallel_for_all_pixels([&] (itk::RGBPixel<double>& p, int x, int y) {
+            Region r = gen_region(x * region_size, y * region_size, region_size, region_size);
+
+            p = get_mean(mixmax, r);
+        });
+        delete mixmax;
 
         return out;
     }
--- Test/test_mixmax.cpp
+++ Test/test_mixmax.cpp
@@ -6,24 +6,23 @@
 using namespace ASTex;
 
 int main() {
-    MixMax mixmax;
-
     ImageRGBd T1, T2;
     ImageGrayd S1, S2, V;
+    IO::loadu8_in_01(T1, TEMPO_PATH+"/PBRMaterials/dirt_color.png");
+    IO::loadu8_in_01(T2, TEMPO_PATH+"/PBRMaterials/paving_color.png");
+    IO::loadu8_in_01(S1, TEMPO_PATH+"/PBRMaterials/dirt_height.png");
+    IO::loadu8_in_01(S2, TEMPO_PATH+"/PBRMaterials/paving_height.png");
+    IO::loadu8_in_01(V, TEMPO_PATH+"/Mask1.png");
+    ImageRGBd *OUT;
 
-    IO::loadu8_in_01(T1, TEMPO_PATH+"/PBRMaterials/grass_color.png");
-    IO::loadu8_in_01(T2, TEMPO_PATH+"/PBRMaterials/rock_color.png");
-    IO::loadu8_in_01(S1, TEMPO_PATH+"/PBRMaterials/grass_height.png");
-    IO::loadu8_in_01(S2, TEMPO_PATH+"/PBRMaterials/rock_height.png");
-    IO::loadu8_in_01(V, TEMPO_PATH+"/Mask2.png");
-
-    std::cout << S1.pixelAbsolute(0,0) << std::endl;
-    std::cout << S1.pixelAbsolute(484,658) << std::endl;
-    std::cout << S1.pixelAbsolute(918,33) << std::endl;
-
-    ImageRGBd *OUT = mixmax.blend(&T1, &T2, &S1, &S2, &V, 0.01, 0.4);
-
-    IO::save01_in_u8(*OUT, TEMPO_PATH+"test_mixmax.png");
+    for(int i = 0; i <= 8; i++) {
+        OUT = MixMax::blend(&T1, &T2, &S1, &S2, &V, i, 0.01, 0.4);
+        IO::save01_in_u8(*OUT, TEMPO_PATH+"test_mixmax_" + std::to_string(i) + ".png");
+        delete OUT;
+        OUT = MixMax::ground_truth(&T1, &T2, &S1, &S2, &V, i, 0.01, 0.4);
+        IO::save01_in_u8(*OUT, TEMPO_PATH+"test_gound_truth_" + std::to_string(i) + ".png");
+        delete OUT;
+    }
 
     return 0;
 }
